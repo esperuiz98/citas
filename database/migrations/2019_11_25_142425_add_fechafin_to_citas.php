96e546b65083a4ee6771793f4af926a72ee40012
claudiacmch <claudiacamachochaparro@gmail.com>
2019-12-04 13:54:47 +0100

    Modificado formato fecha Hecho fecha fin de citas Funciona correctamente

---
+++ database/migrations/2019_11_25_142425_add_fechafin_to_citas.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFechafinToCitas extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+        {
+            Schema::table('citas', function (Blueprint $table) {
+                $table->dateTime('fecha_fin');
+            });
+        }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
